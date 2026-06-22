3b46949d3fc17e540c5254822ded76d79fc6f305
Esubale Endale <esu1011esu@gmail.com>
2026-06-22 18:00:46 +0300

    chore: Wire Assessment and Certificate into the Database

--- Data/Migrations/Add_Assessment_Certificate/20260622142625_AddAssessmentAndCertificate.Designer.cs
+++ Data/Migrations/Add_Assessment_Certificate/20260622142625_AddAssessmentAndCertificate.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TmsApi.Data;
 
 #nullable disable
 
-namespace TmsApi.Data.Migrations
+namespace TmsApi.Data.Migrations.Add_Assessment_Certificate
 {
     [DbContext(typeof(TmsDbContext))]
-    partial class TmsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260622142625_AddAssessmentAndCertificate")]
+    partial class AddAssessmentAndCertificate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
