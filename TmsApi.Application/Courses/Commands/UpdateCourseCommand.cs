c44083b4c115db7de5e113dd44c7cbcc5517d9f9
Esubale-Endale <esu1011esu@gmail.com>
2026-08-28 14:13:22 +0300

    test : The Pure Logic Test (xUnit)

--- TmsApi.Application/Courses/Commands/UpdateCourseCommand.cs
+++ TmsApi.Application/Courses/Commands/UpdateCourseCommand.cs
@@ -5,7 +5,7 @@ namespace TmsApi.Application.Courses.Commands;
 public class UpdateCourseCommand : IRequest<bool>
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public required string Title { get; set; }
     public required string Code { get; set; }
     public int MaxCapacity { get; set; }
 }
